8682f832f371955d3df5499f3de75927a5e9358e
Big Lebowski <gmar@image.ece.ntua.gr>
2018-10-23 17:04:56 +0300

    Create sudirectories for better code organization

--- systemd_add.sh
+++ systemd_add.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-chmod 664 ./agent.service
 PWD=$(pwd)
-sudo ln -s $PWD/agent.service /etc/systemd/system/agent.service
+# Edit Unit file
+CMD=$PWD"/py/agent.py -f "$PWD"/conf/agent.ini"
+STR=`awk -v var="$CMD" '{if(/ExecStart=/) {print$0var}}' $PWD/conf/agent.service`
+echo $STR
+sed -i "s:ExecStart=:$STR:g" $PWD/conf/agent.service
+
+# Link Unit file to systemd dir
+echo "Linking file "$PWD"/py/agent.py to systemd directory"
+chmod 664 conf/agent.service
+sudo ln -s $PWD/conf/agent.service /etc/systemd/system/agent.service
 sudo systemctl daemon-reload
+echo "Done!"
